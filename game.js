43380ba11e4278ab228b4c821c33516bd2023aa3
panzheng <panzheng@baidu.com>
2014-10-31 16:00:51 +0800

    added map

---
+++ game.js
@@ -0,0 +1,73 @@
+(function() {
+	var sources = [
+		'images/grass.png',
+		'images/block.png'
+	];
+	//地图的配置
+	var map = {
+		sources: [
+			'images/grass.png',
+			'images/block.png'
+		],
+		width: 16,
+		height: 24,
+		gridSize: 40,
+		data: [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		]
+	};
+	var mapLayer = document.getElementById('map').getContext('2d');
+	var items = [];
+	var renderMap = function(target, mapData) {
+		console.log(mapData);
+		var _W = mapData.width;
+		var _H = mapData.height;
+		var data = mapData.data;
+		var size = mapData.gridSize;
+		var sources = [];
+		for (var idx = 0; idx < mapData.sources.length; idx++) {
+			var img = new Image();
+			img.src = mapData.sources[idx];
+			sources.push(img);
+		}
+		for (var i = 0; i < _W; i++) {
+			for (var j = 0; j < _H; j++) {
+				var type = data[i][j];
+				target.drawImage(sources[type], i * size, j * size, size, size);
+			}
+		}
+	};
+	var preLoad = function(list, callback) {
+		var count = 0;
+		for (var i = 0; i < list.length; i++) {
+			var img = new Image();
+			img.src = list[i];
+			img.onload = function() {
+				count++;
+				if (count === list.length) {
+					callback();
+				}
+			}
+		}
+	};
+	//开始
+	var start = function() {
+		renderMap(mapLayer, map);
+	};
+	preLoad(sources, start);
+})();
